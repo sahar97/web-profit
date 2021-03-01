2d147df3d804c989f59176e147e3e37d74ff3de0
unknown <you@example.com>
2021-03-01 10:14:15 +0200

    --- Auto-Git Commit ---

--- contact.js
+++ contact.js
@@ -11,153 +11,61 @@ $(document).ready(function() {
 
 
 
-function Fun() {
+function Fun(event) {
+    
     var first_name = document.getElementById("firstName");
-    var last_name = document.getElementById("lastName").value;
-    var email = document.getElementById('email').value;
-    var numb = document.getElementById('numb').value;
-    var com_name = document.getElementById("comName").value;
-    var com_web = document.getElementById('comWeb').value;
-    var j_title = document.getElementById('job').value;
-    var size = document.getElementById('select').value;
+    var last_name = document.getElementById("lastName");
+    var email = document.getElementById('email');
+    var numb = document.getElementById('numb');
+    var com_name = document.getElementById("comName");
+    var com_web = document.getElementById('comWeb');
+    var j_title = document.getElementById('job');
+    var size = document.getElementById('select');
     var letters = /^[a-zA-Z]+$/;
     var e_patt = new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$");
     var n_patt = new RegExp("[0][0-9]{9}");
     var urlRegex = /https?:\/\/[^\s]/;
 
 
-
-//check the first name 
-    if (first_name.value.match(letters)) {
-        first_name.classList.remove("invalid");
-    } else {
+    if(first_name.blur)
+{
+    if (!(first_name.value.match(letters))) {
         first_name.classList.add("invalid");
+        return true ;}
+        else{
+            first_name.classList.remove("invalid");
 
-    }
+        }
+}
+else if(last_name.blur){
+    if (!(last_name.value.match(letters))) {
+        last_name.classList.add("invalid");
+        return true ;}
+        else{
+            last_name.classList.remove("invalid");
 
-//check the last name 
+        }
 
-    if (last_name.value.match(letters)) {
+      alert("butm no click ")
+}
+}
+/*
+//check the first name 
+    if (!(x.value.match(letters))) {
+        x.classList.add("invalid");
+        return true ;
+    } else if(x.value.match(letters)) {
+        first_name.classList.remove("invalid");
         last_name.classList.remove("invalid");
 
-        return true;
-    } else {
-        first_name.classList.add("invalid");
-
-
-        return false;
     }
 
-  }//end of the function 
+  //end of the function 
   
   
+
   
-  
-  
-  function Fun3() {
-    var email = document.getElementById('email').value;
-    var res = patt.test(email);
-  
-    if (!res) {
-        document.getElementById('email').style.border = "1px solid red"
-        document.getElementById('email').style.backgroundImage = "url('./img/invalid.png')";
-        document.getElementById('email').style.backgroundRepeat = "no-repeat";
-        document.getElementById('email').style.backgroundPosition = "right";
-  
-  
-    } else {
-        document.getElementById('email').style.border = "1px solid black"
-        document.getElementById('email').style.backgroundImage = "none";
-    }
-  
-  }
-  
-  
-  
-  
-  function Fun4() {
-    var numb = document.getElementById('numb').value;
-  
-    var res = patt.test(numb);
-  
-    if (!res) {
-        document.getElementById('numb').style.border = "1px solid red"
-        document.getElementById('numb').style.backgroundImage = "url('./img/invalid.png')";
-        document.getElementById('numb').style.backgroundRepeat = "no-repeat";
-        document.getElementById('numb').style.backgroundPosition = "right";
-  
-  
-    } else {
-        document.getElementById('numb').style.border = "1px solid black"
-        document.getElementById('numb').style.backgroundImage = "none";
-    }
-  
-  }
-  
-  
-  function Fun5() {
-    inputtxt = document.getElementById("comName");
-  
-    var letters = /^[a-zA-Z]+$/;
-    if (inputtxt.value.match(letters)) {
-        document.getElementById('comName').style.border = "1px solid black"
-        document.getElementById('comName').style.backgroundImage = "none";
-  
-        return true;
-    } else {
-        document.getElementById('comName').style.border = "1px solid red"
-        document.getElementById('comName').style.backgroundImage = "url('./img/invalid.png')";
-        document.getElementById('comName').style.backgroundRepeat = "no-repeat";
-        document.getElementById('comName').style.backgroundPosition = "right";
-  
-        return false;
-    }
-  }
-  
-  
-  
-  function Fun6() {
-    var inputtxt = document.getElementById('comWeb').value;
-  
-  
-  
-  
-    if (inputtxt.match(urlRegex)) {
-        document.getElementById('comWeb').style.border = "1px solid black"
-        document.getElementById('comWeb').style.backgroundImage = "none";
-  
-        return true;
-    } else {
-        document.getElementById('comWeb').style.border = "1px solid red"
-        document.getElementById('comWeb').style.backgroundImage = "url('./img/invalid.png')";
-        document.getElementById('comWeb').style.backgroundRepeat = "no-repeat";
-        document.getElementById('comWeb').style.backgroundPosition = "right";
-  
-        return false;
-    }
-  }
-  
-  
-  
-  function Fun7() {
-    var j_title = document.getElementById('job').value;
-  
-    if (j_title == "" || j_title == null) {
-        document.getElementById('job').style.border = "1px solid red"
-        document.getElementById('job').style.backgroundImage = "url('./img/invalid.png')";
-        document.getElementById('job').style.backgroundRepeat = "no-repeat";
-        document.getElementById('job').style.backgroundPosition = "right";
-  
-  
-    } else {
-        document.getElementById('job').style.border = "1px solid black"
-        document.getElementById('job').style.backgroundImage = "none";
-    }
-  }
-  
-  
-  
-  
+/*
   function Fun8() {
     var size = document.getElementById('select').value;
   
@@ -168,7 +76,7 @@ function Fun() {
     } else {
         $("#select").css('border', '2px solid blacks');
     }
-  }
+  } */
   
   
   
