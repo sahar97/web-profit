b51a667c21fdcfeaad65c181741253929415b51d
unknown <you@example.com>
2021-03-01 17:34:30 +0200

    --- Auto-Git Commit ---

--- contact.js
+++ contact.js
@@ -7,10 +7,120 @@ $(document).ready(function() {
     });
   });
   
+function Fun(value) {
+
+    var first_name = document.getElementById("firstName");
+    var last_name = document.getElementById("lastName");
+    var email = document.getElementById('email');
+    var numb = document.getElementById('numb');
+    var com_name = document.getElementById("comName");
+    var com_web = document.getElementById('comWeb');
+    var j_title = document.getElementById('job');
+    var size = document.getElementById('select');
+    var letters = /^[a-zA-Z]+$/;
+    var com_patt = /^[0-9a-zA-Z]+$/;
+
+    var e_patt = new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$");
+    var n_patt = new RegExp("[0][0-9]{9}");
+    var urlRegex = /https?:\/\/[^\s]/;
+    switch( value) {
+    case 1:
+        if (!(first_name.value.match(letters))) {
+            first_name.classList.add('invalid')
+        }
+else{
+    first_name.classList.remove('invalid')
+
+}
+        break;
+
+
+        case 2:
+            if (!(last_name.value.match(letters))) {
+                last_name.classList.add('invalid')
+            }
+    else{
+        last_name.classList.remove('invalid')
+    
+    }
+
+    break; 
 
 
 
 
+    case 3:
+        if (!(email.value.match(e_patt))) {
+            email.classList.add('invalid')
+        }
+else{
+    email.classList.remove('invalid')
+
+}
+
+break;
+
+
+
+
+
+
+case 4:
+    if (!(numb.value.match(n_patt))) {
+        numb.classList.add('invalid')
+    }
+else{
+    numb.classList.remove('invalid')
+
+}
+
+break;
+
+
+
+
+
+
+
+
+
+
+
+        
+
+    case 5:
+        if (!(com_name.value.match(com_patt))) {
+            com_name.classList.add('invalid')
+        }
+else{
+    com_name.classList.remove('invalid')
+
+}
+
+break;
+
+
+
+
+
+
+
+
+    case 6:
+        if (!(com_web.value.match(urlRegex))) {
+            com_web.classList.add('invalid')
+        }
+else{
+    com_web.classList.remove('invalid')
+
+}
+
+break; 
+
+    }
+}
+
+/*
 function Fun() {
     
     var first_name = document.getElementById("firstName");
@@ -26,28 +136,8 @@ function Fun() {
     var n_patt = new RegExp("[0][0-9]{9}");
     var urlRegex = /https?:\/\/[^\s]/;
 
-
-    window.onkeyup = e => {
-        console.log(e.target);  // to get the element
-        console.log(e.target.tagName);  // to get the element tag name alone
-    } 
-
 }
-/*
-//check the first name 
-    if (!(x.value.match(letters))) {
-        x.classList.add("invalid");
-        return true ;
-    } else if(x.value.match(letters)) {
-        first_name.classList.remove("invalid");
-        last_name.classList.remove("invalid");
-
-    }
-
-  //end of the function 
-  
-  
-
+*/
   
 /*
   function Fun8() {
